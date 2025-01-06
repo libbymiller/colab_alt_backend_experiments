8e30461b4fb3d1e413d355a9843fc2e0e8e0c672
BertieCanCodeMaybe <153751539+BertieCanCodeMaybe@users.noreply.github.com>
2025-01-06 12:04:23 +0000

    Moving Live stuff to components, not sure which is the right place

--- app/components/RoomProvider.tsx
+++ app/components/RoomProvider.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import React, { useState, useEffect } from "react";
+import LiveList from './LiveList'
+import LiveObject from './LiveObject'
+import LiveMap from './LiveMap'
+
 
 export default function RoomProvider(roomId) {
 
 	return <div id={roomId} 
-
+		initialStorage={{
+			metadata: new LiveObject(),
+			layerIds: new LiveList(),
+			layers: new LiveMap()
+		}}
 
         />
       
